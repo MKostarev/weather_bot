92b7f21ae02171b24a4f772e57130b307d81b78e
Mikhail Kostarev <117512786+MKostarev@users.noreply.github.com>
2023-03-01 22:58:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def current_weather(q: str = "Chicago", appid: str = APPID) -> dict:
 @bot.message_handler(commands=['start'])
 def start_def(message):
     bot.send_message(message.chat.id, "Этот бот поможет тебе узнать погоду."
-                                      "\nВведиет город пожалусат")
+                                      "\nВведите город пожалусат")
     bot.register_next_step_handler(message, weather_def)
 
 def weather_def(message):
